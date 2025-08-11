e12a3be74392158a7e6a1b96318b145c6ff9a617
Hemanta Rana <magarhemanta145@gmail.com>
2025-08-11 15:27:38 +0545

    first commit

---
+++ online-book-store/src/test/java/com/spring_boot/online/book/store/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot.online.book.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
